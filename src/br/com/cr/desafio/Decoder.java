7644c37e29d9bad4ea5d7a9ca96c5db1f96e018f
Rafael Carvalho <crcarvalho@outlook.com>
2019-04-23 16:44:40 -0300

    Melhorando o codigo

--- src/br/com/cr/desafio/Decoder.java
+++ src/br/com/cr/desafio/Decoder.java
@@ -1,99 +1,72 @@
 package br.com.cr.desafio;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
 public class Decoder {
 	
-	private Map<String, Integer> charInt = new HashMap<>();
-	private Map<Integer, String> intChar = new HashMap<>();
+	private ArrayList<String> caracteres = new ArrayList<>();
 	
 	public Decoder() {
-		intChar.put(1, "a");
-		intChar.put(2, "b");
-		intChar.put(3, "c");
-		intChar.put(4, "d");
-		intChar.put(5, "e");
-		intChar.put(6, "f");
-		intChar.put(7, "g");
-		intChar.put(8, "h");
-		intChar.put(9, "i");
-		intChar.put(10, "j");
-		intChar.put(11, "k");
-		intChar.put(12, "l");
-		intChar.put(13, "m");
-		intChar.put(14, "n");
-		intChar.put(15, "o");
-		intChar.put(16, "p");
-		intChar.put(17, "q");
-		intChar.put(18, "r");
-		intChar.put(19, "s");
-		intChar.put(20, "t");
-		intChar.put(21, "u");
-		intChar.put(22, "v");
-		intChar.put(23, "w");
-		intChar.put(24, "x");
-		intChar.put(25, "y");
-		intChar.put(26, "z");
-		
-		charInt.put("a", 1);
-		charInt.put("b", 2);
-		charInt.put("c", 3);
-		charInt.put("d", 4);
-		charInt.put("e", 5);
-		charInt.put("f", 6);
-		charInt.put("g", 7);
-		charInt.put("h", 8);
-		charInt.put("i", 9);
-		charInt.put("j", 10);
-		charInt.put("k", 11);
-		charInt.put("l", 12);
-		charInt.put("m", 13);
-		charInt.put("n", 14);
-		charInt.put("o", 15);
-		charInt.put("p", 16);
-		charInt.put("q", 17);
-		charInt.put("r", 18);
-		charInt.put("s", 19);
-		charInt.put("t", 20);
-		charInt.put("u", 21);
-		charInt.put("v", 22);
-		charInt.put("w", 23);
-		charInt.put("x", 24);
-		charInt.put("y", 25);
-		charInt.put("z", 26);
+		caracteres.add("a");   // 0
+		caracteres.add("b");   // 1
+		caracteres.add("c");   // 2
+		caracteres.add("d");   // 3
+		caracteres.add("e");   // 4
+		caracteres.add("f");   // 5
+		caracteres.add("g");   // 6
+		caracteres.add("h");   // 7
+		caracteres.add("i");   // 8
+		caracteres.add("j");   // 9
+		caracteres.add("k");   // 10
+		caracteres.add("l");   // 11
+		caracteres.add("m");   // 12
+		caracteres.add("n");   // 13
+		caracteres.add("o");   // 14
+		caracteres.add("p");   // 15
+		caracteres.add("q");   // 16
+		caracteres.add("r");   // 17
+		caracteres.add("s");   // 18
+		caracteres.add("t");   // 19
+		caracteres.add("u");   // 20
+		caracteres.add("v");   // 21
+		caracteres.add("w");   // 22
+		caracteres.add("x");   // 23
+		caracteres.add("y");   // 24
+		caracteres.add("z");   // 25
 	}
 	
-	public String decodificar(String textoCodificado, int nCasas) {
-		char[] caracteres = textoCodificado.toLowerCase().toCharArray();
+public String decodificar(String textoCodificado, int nCasas) {
+		
+		char[] chars = textoCodificado.toLowerCase().toCharArray();
 		String textoDecodificado = "";
 		
-		for (char c : caracteres) {
-			Integer cont = charInt.get(""+c);
+		for (char c : chars) {
+			// recebe a posicao do caracter atual
+			Integer cont = caracteres.indexOf(""+c)+1;
 			
-			if(cont != null) {
+			// verifica se contador é diferente de 0 (valor retornado para elementos não encontrados no arraylist)
+			if(cont != 0) {
+				// chama o metodo para buscar o caracter correto passando a posicao atual e o decressor
 				textoDecodificado = textoDecodificado+buscaCaracterCorreto(cont, nCasas);
 			}else {
+				// escreve o elemento não localizado no array
 				textoDecodificado = textoDecodificado+c;
 			}
 		}
 		
 		return textoDecodificado;
 	}
-
+	
 	private String buscaCaracterCorreto(Integer cont, int nCasas) {
-		
+		// recebe a posicao descrescida
 		Integer somatorio = cont-nCasas;
 		
-		if(somatorio<0) {
+		// caso a posicao seja negativa
+		if(somatorio<=0) {
 			somatorio += 26;
 		};
 		
-		if(somatorio == 0) {
-			somatorio = 26;
-		}
-		
-		return intChar.get(somatorio);
+		return caracteres.get(somatorio-1);
 	}
 	
 }
